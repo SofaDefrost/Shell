139e2eecce7f5ab5cf21469cce0eeedb39cb84df
duriez <duriez@7b2761c9-2758-49ef-8c29-9c9f1e1aad1b>
2010-03-11 14:01:02 +0000

    demos for paper MICCAI
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/sofa-shells@97 7b2761c9-2758-49ef-8c29-9c9f1e1aad1b

--- examples/conv.sh
+++ examples/conv.sh
@@ -1,1 +1,12 @@
-awk 'BEGIN { print "<Mesh position=\""; section=0 } /Coordinates/ { section=1 } (NF==4&&section==1) { print $2 " " $3 " " $4 } /Elements/ { section+=1; if (section==2) { print "\"\n tetrahedra=\"" } } (NF==5&&section==2) { print $2-1 " " $3-1 " " $4-1 " " $5-1 } END { print "\" />" }'
+awk 'BEGIN { print "<Mesh position=\""; section=0 } 
+/Coordinates/ { section=section+1 } 
+/Triangle/ {section=2}
+(NF==4&&section==1) { print $2 " " $3 " " $4 } 
+/Elements/ {  
+	     if (section==1) { print "\"\n tetrahedra=\""  } 
+	     if (section==2)  {print "\"\n triangles=\"" } 
+	    } 
+
+(NF==5&&section==1) { print $2-1 " " $3-1 " " $4-1 " " $5-1 } 
+(NF==4&&section==2) {print $2-1 " " $4-1 " " $3-1 }
+END { print "\" />" }'
