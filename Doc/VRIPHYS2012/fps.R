dc271dfebdb1210d0e7c3d8fc0a4a9bd9ffe1b49
nyoxi <nyoxi@7b2761c9-2758-49ef-8c29-9c9f1e1aad1b>
2012-08-01 17:08:09 +0000

    fixing FPS for the test (only visual mapping)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/sofa-shells@442 7b2761c9-2758-49ef-8c29-9c9f1e1aad1b

--- Doc/VRIPHYS2012/fps.R
+++ Doc/VRIPHYS2012/fps.R
@@ -7,13 +7,13 @@ fps <- read.csv("fps.csv");
 pdf("fps.pdf", width=7, height=4);
 p <- par(lwd=2, mar=c(4,4,1,1)+0.1);
 
-plot(range(fps$elements), range(c(fps$fpsBase, fps$fpsPolar, fps$fpsMap1, fps$fpsMap2)),
+plot(range(fps$elements), range(c(fps$fpsBase, fps$fpsPolar, fps$fpsMap1, fps$fpsMap2), na.rm=TRUE),
     type="n", xlab="Elements", ylab="FPS", log="y");
 
 lines(fps$elements, fps$fpsBase, type="b", col="black", pch=1);
 lines(fps$elements, fps$fpsPolar, type="b", col="black", pch=4);
-lines(fps$elements, fps$fpsMap2, type="b", col="black", pch=5);
-lines(fps$elements, fps$fpsMap1, type="b", col="black", pch=6);
+lines(fps$elements, fps$fpsMap1, type="b", col="black", pch=5);
+lines(fps$elements, fps$fpsMap2, type="b", col="black", pch=6);
 
 abline(h=25, lty="dotted");
 
