a58dce313d7be25feaf795dbe3c0a0ff670d15dc
nyoxi <nyoxi@7b2761c9-2758-49ef-8c29-9c9f1e1aad1b>
2013-02-28 23:57:34 +0000

    bibliography, comments, figures for aspiration
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/sofa-shells@530 7b2761c9-2758-49ef-8c29-9c9f1e1aad1b

--- Doc/Miccai2013/aspiration.R
+++ Doc/Miccai2013/aspiration.R
@@ -1,14 +1,14 @@
 a1 <- data.matrix(read.csv("aspiration-simple.csv"))
 a2 <- data.matrix(read.csv("aspiration-composite.csv"))
 
-a1 <- a1[1:21,c(4,3)]; a1[,2] <- a1[,2] - 0.03
-a2 <- a2[1:21,c(4,3)]; a2[,2] <- a2[,2] - 0.03
+a1 <- a1[1:31,c(4,3)]; a1[,2] <- a1[,2] - 0.015; a1[,2] <- a1[,2] * 1000
+a2 <- a2[1:33,c(4,3)]; a2[,2] <- a2[,2] - 0.015; a2[,2] <- a2[,2] * 1000
 
-pdf("aspiration.pdf", width=4, height=2, pointsize=9); # sizes in inches
+pdf("aspiration.pdf", width=3, height=1.5, pointsize=8); # sizes in inches
 p <- par(lwd=2, mar=c(4,4,1,1)+0.1);
 
 plot(range(c(a1[,1], a2[,1])), range(c(a1[,2], a2[,2])), #ylim=c(0, 0.004), 
-     type="n", xlab="x-coordinate", ylab="Displacement")
+     type="n", xlab="x-coordinate", ylab="Displacement [mm]")
 
 lines(a1, col="black")
 lines(a2, col="blue")
