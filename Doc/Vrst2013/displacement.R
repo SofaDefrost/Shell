82fe5b30d56d46a9e84e58ffbada4dc535f03363
nyoxi <nyoxi@7b2761c9-2758-49ef-8c29-9c9f1e1aad1b>
2013-05-31 20:26:43 +0000

    changing the time range
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/sofa-shells@593 7b2761c9-2758-49ef-8c29-9c9f1e1aad1b

--- Doc/Vrst2013/displacement.R
+++ Doc/Vrst2013/displacement.R
@@ -8,8 +8,8 @@ mc[,3] <- (mc[,3] - 0.015)*1000
 pdf("displacement.pdf", width=3, height=1.5, pointsize=8); # sizes in inches
 p <- par(lwd=0.5, mar=c(4,4,0,0)+0.1);
 
-i <- 1:500 # 5 seconds
-plot(c(0,5),
+i <- 1:200 # 5 seconds
+plot(c(0,2),
      c(-0.285, 2.336), # make the range consistent
      #range(mt[,3], mc[,3]),
      type="n", xlab="Time [s]", ylab="Displacement [mm]")
